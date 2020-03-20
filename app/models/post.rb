638d783c15b97301c2ff3cc77a4d2085c199f7b1
laarchambault <laarchambault@gmail.com>
2020-03-20 09:41:22 -0400

    still need to fix clickbait validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_must_be_clickbaity
+
+    def title_must_be_clickbaity
+        if !title.include? "Won't Believe" || "Secret" || "Top " || "Guess"
+            errors.add :title, "Title is not clickbait-y enough"
+        end
+    end
+    #If the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess",
 end
